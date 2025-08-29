70138936c8dcc899cba69dbd1698fe5cb60d3475
Davi Lima <devDavi64lima@gmail.com>
2025-08-29 15:52:59 -0300

    The minimum to have a GraphQL schema

--- src/schema/schema.ts
+++ src/schema/schema.ts
@@ -1,3 +1,11 @@
 import { GraphQLSchema } from "graphql";
 
-export const schema = new GraphQLSchema({});
+import { QueryType } from "./QueryType";
+// import { MutationType } from "./MutationType";
+// import { SubscriptionType } from "./SubscriptionType";
+
+export const schema = new GraphQLSchema({
+  query: QueryType,
+  //   mutation: MutationType,
+  //   subscription: SubscriptionType,
+});
