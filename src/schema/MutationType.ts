1f892271b5af1bfbf6fb6eeafb07ed666176a4a4
Davi Lima <devDavi64lima@gmail.com>
2025-09-01 12:16:32 -0300

    refactor: restructure GraphQL schema and move to modules

--- src/schema/MutationType.ts
+++ src/schema/MutationType.ts
@@ -1,5 +1,5 @@
 import { GraphQLObjectType } from "graphql";
-import { createAccountMutation } from "graphql/Account/mutations/createAccount";
+import { createAccountMutation } from "modules/Account/mutations/createAccount";
 
 export const MutationType = new GraphQLObjectType({
   name: "Mutation",
--- src/schema/QueryType.ts
+++ src/schema/QueryType.ts
@@ -1,8 +1,8 @@
 import { GraphQLObjectType } from "graphql";
-import { accountQuery } from "graphql/Account/queries/account";
-import { accountsQuery } from "graphql/Account/queries/accounts";
-import { transactionsQuery } from "graphql/Transaction/queries/transactions";
-import { transactionQuery } from "graphql/Transaction/queries/transaction";
+import { accountQuery } from "modules/Account/queries/account";
+import { accountsQuery } from "modules/Account/queries/accounts";
+import { transactionsQuery } from "modules/Transaction/queries/transactions";
+import { transactionQuery } from "modules/Transaction/queries/transaction";
 
 export const QueryType = new GraphQLObjectType({
   name: "Query",
