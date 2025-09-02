f1792247fbbc26f6c0c5817ba9bf339e24e84cf8
Davi Lima <devDavi64lima@gmail.com>
2025-09-02 06:51:27 -0300

    feat: enhance AccountType with additional fields and transaction support

--- src/modules/Account/Account.ts
+++ src/modules/Account/Account.ts
@@ -5,12 +5,48 @@ import {
   GraphQLObjectType,
   GraphQLString,
 } from "graphql";
+import { Account } from "models";
+import { globalIdField } from "graphql-relay";
+import { accountQuery } from "./queries/account";
 
 export const AccountType = new GraphQLObjectType({
   name: "Account",
-  fields: {
-    id: { type: new GraphQLNonNull(GraphQLID) },
-    name: { type: new GraphQLNonNull(GraphQLString) },
-    balance: { type: new GraphQLNonNull(GraphQLInt) },
-  },
+  fields: () => ({
+    id: globalIdField("account"),
+    publicId: {
+      type: GraphQLID,
+      resolve: (account) => account.publicId,
+    },
+    userId: {
+      type: GraphQLString,
+      resolve: (account) => account.userId,
+    },
+    user: {
+      type: UserType,
+      resolve: (account) => account.user,
+    },
+    balance: {
+      type: GraphQLInt,
+      resolve: (account) => account.balance,
+    },
+    transactions: {
+      type: TransactionConnection,
+      args: connectionArgs,
+      description: "List of transactions for the account",
+      resolve: async (account, args) => {
+        console.log(account._id);
+
+        const transactions = await transactionDataLoader.load(account._id);
+        return connectionFromArray(transactions, args);
+      },
+    },
+    createdAt: {
+      type: GraphQLString,
+      resolve: (account) => account.createdAt,
+    },
+    updatedAt: {
+      type: GraphQLString,
+      resolve: (account) => account.updatedAt,
+    },
+  }),
 });
